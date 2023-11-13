a2e2096b57a0c68ef6f9f5686f3f0161a54562a7
najem <mohamadnejmeddine@gmail.com>
2023-11-13 16:30:25 +0100

    first commit

---
+++ Js_note.js
@@ -0,0 +1,61 @@
+let title="notepad";
+let descrption=
+    `Lorem ipsum is placeholder
+    text commonly used in
+    the graphic, print`;
+let date="25/11";
+let markup = `
+    <div class="card">
+        <div class="child">
+            <h3>${title}</h3>
+            <p>${descrption}</p> 
+            <span>${date}</span>
+        </div>
+    </div>`;  
+    document.write(`${markup} `);
+
+
+
+console.log(parseInt("568.395"));
+console.log(parseFloat("568.395"));
+
+console.log(math.random([15,78,69,25,97,39,20,56,78,39,10]));
+
+
+
+let date_2 = String(new Date());
+    document.write(date_2);
+
+    console.log(100);
+    console.log(+"215");
+    console.log(+"0xff");
+    console.log(+null);
+    console.log(+true);
+    console.log(+false);
+    console.log(-("-55"));
+    console.log(-(false));
+    console.log(-(true));
+    console.log(-null);
+    console.log(-0);
+    console.log(100);
+    console.log(+"215");
+    console.log(+"0xff");
+    console.log(+null);
+    console.log(+true);
+    console.log(+false);
+    console.log(-("-55"));
+    console.log(-(false));
+    console.log(-(true));
+    console.log(-null);
+    console.log(-0);
+    console.log(100);
+    console.log(+"215");
+    console.log(+"0xff");
+    console.log(+null);
+    console.log(+true);
+    console.log(+false);
+    console.log(-("-55"));
+    console.log(-(false));
+    console.log(-(true));
+    console.log(-null);
+    console.log(-0);
